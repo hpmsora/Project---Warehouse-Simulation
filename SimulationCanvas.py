0476708debcf40d41fe0e427cd7cdd2cd1051a8d
Won Yong Ha <wh346@cornell.edu>
2020-02-04 18:01:42 -0500

    Basic structure update

---
+++ SimulationCanvas.py
@@ -0,0 +1,25 @@
+###############################
+#
+# Simulation Canvas
+#
+# Won Yong Ha
+#
+#
+###############################
+
+import tkinter as tk
+
+class SimulationBoardx:
+    def SimulationBoard(self, 
+    
+    def RootBuilding():
+        root = tk.Tk()
+        root.title("Warehouse Simulation")
+        
+        canvas = tk.Canvas(root, width=500, height=500, bg='white')
+        canvas.pack()
+        
+        return root
+
+    def RootLooping(_root):
+        _root.mainloop()
---
+++ main.py
@@ -0,0 +1,19 @@
+###############################
+#
+# Main
+#
+# Won Yong Ha
+#
+#
+###############################
+
+import SimulationCanvas as SC
+
+def main():
+
+    # Draw Canvas
+    Root = SC.RootBuilding()
+    SC.RootLooping(Root)
+
+if __name__ == "__main__":
+    main()
